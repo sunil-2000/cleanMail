f52a207e596f0517f6f4cda0fb96496a7dcbd661
sunil-2000 <sunilsabnis10@gmail.com>
2022-07-21 14:38:30 -0500

    bug fixes for kmeans/tfidf and pickling mecahnism implementation

--- message.py
+++ message.py
@@ -1,105 +1,136 @@
 import numpy as np
 import torch
 import torchtext
-import string 
-import nltk 
+import string
+import nltk
 import re
 
 try:
-  from nltk.tokenize import word_tokenize
-  from nltk.corpus import stopwords
-  from nltk.stem import WordNetLemmatizer
+    from nltk.tokenize import word_tokenize
+    from nltk.corpus import stopwords
+    from nltk.stem import WordNetLemmatizer
 except:
-  nltk.download('wordnet')
-  nltk.download('stopwords')
-  nltk.download('omw-1.4')
-  nltk.download('punkt')
+    nltk.download("wordnet")
+    nltk.download("stopwords")
+    nltk.download("omw-1.4")
+    nltk.download("punkt")
 
-class Message():
-  """
-  Represents a message (subject, body, sender) data
-  """
-  def __init__(self, subject, body, sender, uid, dim=100, gen_features=True):
-    self.subject = subject
-    self.raw_subject = subject
-    self.body = body
-    self.sender = sender 
-    self.uid = uid
-    # trained on Wikipedia 2014 corpus; can try other models
-    self.dim = dim
-    self.glove = torchtext.vocab.GloVe(name="6B",
-                              dim=dim)
 
-    self.body_feature, self.subject_feature = None, None
+class Message:
+    """
+    Represents a message (subject, body, sender) data
 
-    if gen_features:
-      self.preprocess()
-      self.feature_extraction()
+    attributes:
+    subject (str): processed/cleaned, subject of message
+    raw_subject (str): original uncleaned subject str
+    body (str): processed/cleaned, body of message
+    sender (str): sender email address of message
+    uid (str): uid of email
+    keywords (None or list): initialized as None, but can be populated with
+    list of keywords of message after calling tfidf from mail object
+    glove (torch model): GloVe model used to convert words to vectors
+    body_feature (np array): vector that represents body of message
+    subject_feature (np array): vector that represents subject of message
+    """
 
-  def _remove_stop_words(self) -> None:
-    """
-    word must be tokenized
-    remove common english stopwords
-    """
-    assert type(self.subject) == list and type(self.body) == list
-    stop_words = set(stopwords.words('english'))
-    self.subject = [w for w in self.subject if not w in stop_words]
-    self.body = [w for w in self.body if not w in stop_words]
+    def __init__(self, subject, body, sender, uid, dim=100):
+        """
+        subject (str): subject string of message
+        raw_subject (str): subject string of message
+        body (str): body string of message
+        sender (str): sender email address of message
+        uid (str): uid of email
+        dim (int): dimension of glove vector / dimensionality of features generated
+        by glove
+        """
+        self.subject = subject
+        self.raw_subject = subject
+        self.body = body
+        self.sender = sender
+        self.uid = uid
+        self.keywords = None
+        # trained on Wikipedia 2014 corpus; can try other models
+        self.glove = torchtext.vocab.GloVe(name="6B", dim=dim)
 
-  def preprocess(self) -> None:
-    """
-    performs preprocessing steps on raw message
-    """
-    # lower case
-    self.body, self.subject = self.body.lower(), self.subject.lower()
-    # remove punctuation
-    self.body = self.body.translate(str.maketrans('', '', string.punctuation))
-    self.subject = self.subject.translate(str.maketrans('', '',string.punctuation))
-    # remove digits
-    self.body = re.sub(r'\d+', '', self.body)
-    self.subject = re.sub(r'\d+', '', self.subject)
-    # tokenize
-    self.body = word_tokenize(self.body)
-    self.subject = word_tokenize(self.subject)
-    # remove stop words
-    # print(type(self.body) == list, type(self.subject) == list)
-    self._remove_stop_words()
-    # Lemmatize 
-    wnl = WordNetLemmatizer()
-    self.body = [wnl.lemmatize(w) for w in self.body]
-    self.subject = [wnl.lemmatize(w) for w in self.subject]
-    # remove non-ascii characters ? 
-  
-  def get_top_k_words(self, txt, k, corpus):
-    """
-    returns top K words using TF-IDF algorithm 
-    """
-    # *** requires building corpora (vocab) of all emails in inbox
-    # make corpora a global var (potentially)
-    pass 
+        self.body_feature, self.subject_feature = None, None
+        self.preprocess()
+        self.feature_extraction()
 
-  def feature_extraction(self) -> None:
-    """
-    method to convert message into feature (vectorize)
-    corpus: full vocabulary of email inbox 
-    """
-    assert type(self.subject) == list and type(self.body) == list
+    def _remove_stop_words(self):
+        """
+        helper function for preprocess
+        word must be tokenized
+        remove common english stopwords
+        """
+        assert type(self.subject) == list and type(self.body) == list
+        stop_words = set(stopwords.words("english"))
+        self.subject = [w for w in self.subject if not w in stop_words]
+        self.body = [w for w in self.body if not w in stop_words]
 
-    self.body_feature = self.glove.get_vecs_by_tokens(self.body, lower_case_backup=True).numpy()
-    self.subject_feature = self.glove.get_vecs_by_tokens(self.subject, lower_case_backup=True).numpy()
-    
-    self.body_feature = np.sum(self.body_feature, axis=0)
-    self.subject_feature = np.sum(self.subject_feature, axis=0)
+    def preprocess(self):
+        """
+        performs preprocessing steps on raw message
+        """
+        # lower case
+        self.body, self.subject = self.body.lower(), self.subject.lower()
+        # remove punctuation
+        self.body = self.body.translate(str.maketrans("", "", string.punctuation))
+        self.subject = self.subject.translate(str.maketrans("", "", string.punctuation))
+        # remove digits
+        self.body = re.sub(r"\d+", "", self.body)
+        self.subject = re.sub(r"\d+", "", self.subject)
+        # tokenize
+        self.body = word_tokenize(self.body)
+        self.subject = word_tokenize(self.subject)
+        # remove unicode chars
+        self.body = [(s.encode("ascii", "ignore")).decode("utf-8") for s in self.body]
+        self.subject = [
+            (s.encode("ascii", "ignore")).decode("utf-8") for s in self.subject
+        ]
+        self.body = [s for s in self.body if s != ""]
+        self.subject = [s for s in self.body if s != ""]
+        # remove stop words
+        self._remove_stop_words()
+        # Lemmatize
+        wnl = WordNetLemmatizer()
+        self.body = [wnl.lemmatize(w) for w in self.body]
+        self.subject = [wnl.lemmatize(w) for w in self.subject]
 
-  def print_closest_words(self, vec, n=5):
-    dists = torch.norm(self.glove.vectors - vec, dim=1) 
-    lst = sorted(enumerate(dists.numpy()), key=lambda x: x[1]) # sort by distance
-    for idx, difference in lst[1:n+1]: 	# take the top n
-        print(self.glove.itos[idx], difference)
+    def get_top_k_words(self):
+        """
+        returns top K words using TF-IDF algorithm,
+        only returns keywords if tfidf called from mail object
+        """
+        return self.keywords
 
-  def get_cleaned_body(self) -> list:
-    """
-    return cleaned body of email
-    """
-    assert type(self.body) == list
-    return self.body
+    def feature_extraction(self):
+        """
+        method to convert message into feature (vectorize) using GloVe
+        """
+        assert type(self.subject) == list and type(self.body) == list
+
+        self.body_feature = self.glove.get_vecs_by_tokens(
+            self.body, lower_case_backup=True
+        ).numpy()
+        self.subject_feature = self.glove.get_vecs_by_tokens(
+            self.subject, lower_case_backup=True
+        ).numpy()
+
+        self.body_feature = np.sum(self.body_feature, axis=0)
+        self.subject_feature = np.sum(self.subject_feature, axis=0)
+
+    def print_closest_words(self, vec, n=5):
+        """
+        prints closest word to a given GloVe vector
+        """
+        dists = torch.norm(self.glove.vectors - vec, dim=1)
+        lst = sorted(enumerate(dists.numpy()), key=lambda x: x[1])  # sort by distance
+        for idx, difference in lst[1 : n + 1]:  # take the top n
+            print(self.glove.itos[idx], difference)
+
+    def get_cleaned_body(self):
+        """
+        return cleaned body of email
+        """
+        assert type(self.body) == list
+        return self.body
--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,7 @@
 from mail import Mail
-from message import Message
+import pprint
+from os import path
+
 # testing run script
 # 1. pull emails
 # 2. preprocess / clean text
@@ -7,41 +9,29 @@ from message import Message
 # 4. stack text vectors into matrix 
 # 5. run clustering algorithm 
 
-# # # run script
-mail = Mail()
-
-# method performs pre-processing on each email text (subject/body)
-messages = mail.get_messages(msg_count=50) # 50 api requests
-
-# stack each message vector into matrix
-mail.generate_mail_matrix()
-
-# perform kMeans
-# mail.k_means()
-mail.tf_idf(20)
-# print(out)
+if path.exists('mail_pickle.pkl'):
+  # unpickle 
+  print("object has been cached... unpickling")
+  mail = Mail.unpickle_obj('mail_pickle.pkl')
+  # stack each message vector into matrix
+  mail.generate_mail_matrix(True)
+  # run models 
+  mail.k_means()
+  mail.tf_idf(5)
+  # do something with output of model(s)
+  pprint.pprint(mail.keywords_by_msg)
+else:
+  mail = Mail()
+  # method performs pre-processing on each email text (subject/body)
+  messages = mail.get_messages(msg_count=50) # 50 api requests
+  # stack each message vector into matrix
+  mail.generate_mail_matrix(True)
+  # run models 
+  mail.k_means()
+  mail.tf_idf(5)
+  # do something with output of model(s)
+  pprint.pprint(mail.keywords_by_msg)
+  # pickle object
+  Mail.pickle_obj(mail, "mail_pickle.pkl")
 
 # dump results in text file
-
-
-##### NEED TO CREATE METHOD TO PICKLE / CACHE emails , so don't have to 
-# run script everytime to get same data 
-
-
-
-# # build corpus IF we want to perform Tf-Idf on body text (might be unnecessary)
-# because corpus still relatively small 
-# corpus = []
-# for m in mail.messages:
-#   # after each m has been cleaned 
-#   corpus.extend(m.body)
-
-# for m in mail.messages:
-#   # perform feature extraction 
-#   m.feature_extraction(corpus)
-
-
-# print(mail.messages)
-
-
-# get emails, test feature eng pipeline, cluster
